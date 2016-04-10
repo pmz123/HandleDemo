a2c8efa4b4a581ba1349264d924aff80ee9d314c
pmz_kobe <pmz_kobe@163.com>
2016-04-10 17:56:23 +0800

    SDSDSDSD

--- app/src/main/java/com/example/pmz/handledemo/MainActivity.java
+++ app/src/main/java/com/example/pmz/handledemo/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView= (ImageView) findViewById(R.id.imageView);
+        imageView= (ImageView) findViewById(R.id.imageView);//HHHH
 //        new Thread(new Runnable() {
 //            @Override
 //            public void run() {
